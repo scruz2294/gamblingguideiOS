3fa8595857d09082749a854effd3d7cfd732b5a5
Stephanie Cruz <scruz2294@ufl.edu>
2016-06-29 15:53:36 -0400

    initial commit, created all views and controllers

---
+++ GamblingGuideforiOS/GamblingGuideforiOS/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  GamblingGuideforiOS
+//
+//  Created by Stephanie Cruz on 6/29/16.
+//  Copyright © 2016 Stephanie Cruz. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToGames(segue:UIStoryboardSegue) {
+        // Unwind segue to the Games View
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GamblingGuideforiOS/GamblingGuideforiOS/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  GamblingGuideforiOS
+//
+//  Created by Stephanie Cruz on 6/29/16.
+//  Copyright © 2016 Stephanie Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToHome(segue:UIStoryboardSegue) {
+        // Unwind home segue
+    }
+
+
+}
+
